ffae423908e13ea886b3453abd254fb6a470fd37
Md Naeem Mia <mdnaeemm401@gmail.com>
2024-05-13 23:33:02 +0300

    Some Error fixed

--- index.js
+++ index.js
@@ -26,13 +26,12 @@ async function run() {
     const volunteerPostCollection = client.db('volunteerSphere').collection('volunteers')
     const volunteersCollection = client.db('volunteerSphere').collection('dummyVolunteers')
 
-    app.post('/', async(req, res) => {
+    app.post('/posts', async(req, res) => {
         const volunteersPost = req.body;
         const result = await volunteerPostCollection.insertOne(volunteersPost)
         res.send(result)
     })
-
-    app.get('/', async(req, res) => {
+    app.get('/posts', async(req, res) => {
       const searchTerm = req.query.title;
 
       let query = {}; 
@@ -44,19 +43,14 @@ async function run() {
       res.json(result);
     })
      // Get a single data from db using id
-     app.get('/:id', async (req, res) => {
+     app.get('/post/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await volunteerPostCollection.findOne(query)
       res.send(result)
     })
 
-    //get 6 data used in home
-    app.get('/home', async (req, res) => {
-      const result = await volunteerPostCollection.find().limit(6).sort({ deadline: 1 }).toArray();
-
-        res.json(result);
-    })
+   
   
     app.get('/dummyVolunteers', async(req, res) => {
       const result = await volunteersCollection.find().toArray()
