9f9607455d57983baef915760095225b25e9f567
Md Naeem Mia <mdnaeemm401@gmail.com>
2024-05-13 03:13:49 +0300

    Get Volunteer Nedd Details route by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-//volunteerSphere 
-//i3ZkFyRrs1OfdtDP
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.47zrhkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,8 +33,21 @@ async function run() {
     })
 
     app.get('/', async(req, res) => {
-        const result = await volunteerPostCollection.find().toArray()
+      const searchTerm = req.query.title;
 
+      let query = {}; 
+      if (searchTerm) {
+        query = { title: { $regex: searchTerm, $options: 'i' } };
+      }
+
+      const result = await volunteerPostCollection.find(query).sort({ deadline: 1 }).toArray();
+      res.json(result);
+    })
+     // Get a single data from db using id
+     app.get('/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await volunteerPostCollection.findOne(query)
       res.send(result)
     })
 
